c542df3ae6a3b0e0a0956fd463610ce032608f5b
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-10-28 18:33:34 -0600

    First test with sundown library.

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
+#include "markdown.h"
+#include "html.h"
+#include "buffer.h"
+
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     printf("Content-type: text/html\n\n");
+
+    char* test_md = "abc\n===\n\n* a\n* b\n";
+
+    struct sd_callbacks callbacks;
+    struct html_renderopt options;
+    sdhtml_renderer(&callbacks, &options, 0);
+    struct sd_markdown* md = sd_markdown_new(0, 16, &callbacks, &options);
+    struct buf* ob = bufnew(64);
+    sd_markdown_render(ob, test_md, strlen(test_md), md);
+    
     printf("abc\n");
+    printf("%s\n", ob->data);
     return 0;
 }
 
