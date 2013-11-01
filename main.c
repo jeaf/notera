1d6765111c8e61ce203fc846036a57502241ad46
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-11-01 16:49:24 -0600

    Snapshot

--- main.c
+++ main.c
@@ -16,6 +16,10 @@
 sqlite3* db        = NULL;
 sqlite3_stmt* stmt = 0;
 
+const char* login_page = "<html><head><script src=\"http://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js\"></script></head><body><form name=\"login_form\" action=\"http://localhost:8000/main.exe?login=1\" method=\"post\">            Username: <input type=\"text\"     name=\"usr\"><br/>            Password: <input type=\"password\" name=\"pwd\"><br/>            <input type=\"submit\" value=\"Login\" />        </form>    </body></html>";
+
+
+
 int utf8_to_md()
 {
     char* test_md = "abc\n===\n\n* a\n* b\n";
@@ -76,9 +80,23 @@ int main()
          db,
          "CREATE TABLE IF NOT EXISTS session("
          "    sid           INTEGER PRIMARY KEY,"
-         "    creation_time INTEGER NOT NULL DEFAULT (strftime('%s', 'now')));",
+         "    creation_time INTEGER NOT NULL DEFAULT (strftime('%s', 'now')));"
+         "CREATE TABLE IF NOT EXISTS user("
+         "    id   INTEGER PRIMARY KEY,"
+         "    name TEXT,"
+         "    pwd  TEXT);",
          0, 0, 0);
     CHECK(rc == SQLITE_OK, "Can't create tables: %d", rc)
+
+    // Print stdin
+    //char buf[1024] = {0};
+    //long len       = fread(buf, 1, 1024, stdin);
+    //printf("%s", buf);
+    //while (!feof(stdin))
+    //{
+    //    len = fread(buf, 1, 1024, stdin);
+    //    printf("%s", buf);
+    //}
 
     // Check SID
     bool authenticated = false;
@@ -105,10 +123,18 @@ int main()
     }
 
     printf("HTTP/1.0 200 OK\n");
-    printf("Content-type: text/html\n");
+    printf("Content-type: text/html\n\n");
+    printf("<p>QUERY_STRING: %s</p>", getenv("QUERY_STRING"));
+
+    if (strstr(getenv("QUERY_STRING"), "login"))
+    {
+        printf("login\n");
+        authenticated = true;
+    }
 
     if (authenticated)
     {
+        printf("logged in!\n");
     }
     else
     {
@@ -116,9 +142,63 @@ int main()
         printf("Set-Cookie: sid=%lld; Max-Age=60\n", sid);
 
         // Send the login page
-        // todo
+        printf("%s\n", login_page);
     }
 
+    printf("\n");
+    printf("CONTENT_LENGHT: %s\n", getenv("CONTENT_LENGTH"));
+
+    // Print stdin
+    char buf[1024] = {0};
+    long len       = fread(buf, 1, 1024, stdin);
+    printf("len: %d, str: %s\n", len, buf);
+    while (!feof(stdin))
+    {
+        len = fread(buf, 1, 1024, stdin);
+        printf("%s\n", buf);
+    }
     return 0;
 }
 
+// CGI variables
+// AUTH_PASSWORD
+// AUTH_TYPE
+// AUTH_USER
+// CERT_COOKIE
+// CERT_FLAGS
+// CERT_ISSUER
+// CERT_KEYSIZE
+// CERT_SECRETKEYSIZE
+// CERT_SERIALNUMBER
+// CERT_SERVER_ISSUER
+// CERT_SERVER_SUBJECT
+// CERT_SUBJECT
+// CF_TEMPLATE_PATH
+// CONTENT_LENGTH
+// CONTENT_TYPE
+// CONTEXT_PATH
+// GATEWAY_INTERFACE
+// HTTPS
+// HTTPS_KEYSIZE
+// HTTPS_SECRETKEYSIZE
+// HTTPS_SERVER_ISSUER
+// HTTPS_SERVER_SUBJECT
+// HTTP_ACCEPT
+// HTTP_ACCEPT_ENCODING
+// HTTP_ACCEPT_LANGUAGE
+// HTTP_CONNECTION
+// HTTP_COOKIE
+// HTTP_HOST
+// HTTP_REFERER
+// HTTP_USER_AGENT
+// QUERY_STRING
+// REMOTE_ADDR
+// REMOTE_HOST
+// REMOTE_USER
+// REQUEST_METHOD
+// SCRIPT_NAME
+// SERVER_NAME
+// SERVER_PORT
+// SERVER_PORT_SECURE
+// SERVER_PROTOCOL
+// SERVER_SOFTWARE
