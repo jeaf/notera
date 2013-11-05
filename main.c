a82d8cb90f1105a0b4103522da036beea446b0e3
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-11-04 18:24:21 -0600

    Snapshot.

--- main.c
+++ main.c
@@ -8,17 +8,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define CHECK(cond, msg, ...) if (!(cond)) {                  \
-                                  printf(msg, ##__VA_ARGS__); \
-                                  printf("\n");               \
-                                  return 0;}
+#define CHECK(cond, msg, ...) if (!(cond)) error(msg, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
 
 sqlite3* db        = NULL;
 sqlite3_stmt* stmt = 0;
 
-const char* login_page = "<html><head><script src=\"http://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js\"></script></head><body><form name=\"login_form\" action=\"http://localhost:8000/main.exe?login=1\" method=\"post\">            Username: <input type=\"text\"     name=\"usr\"><br/>            Password: <input type=\"password\" name=\"pwd\"><br/>            <input type=\"submit\" value=\"Login\" />        </form>    </body></html>";
+void error(const char* msg, const char* file, const char* func, long line, ...)
+{
+    va_list args;
+    va_start(args, line);
+    printf("HTTP/1.0 200 OK\n");
+    printf("Content-type: text/html\n");
+    printf("\n");
+    printf("<html><head></head><body>");
+    printf("<p>An error occurred while generating this page:</p>\n<p>");
+    vprintf(msg, args);
+    printf("</p>\n<p>At: %s!%s:%d", file, func, line);
+    printf("</p>\n</body></html>\n");
+    exit(0);
+}
 
-
+const char* login_page = "<html><head></head><body><form name=\"login_form\" action=\"http://localhost:8000/main.exe?login=1\" method=\"post\">            Username: <input type=\"text\"     name=\"usr\"><br/>            Password: <input type=\"password\" name=\"pwd\"><br/>            <input type=\"submit\" value=\"Login\" />        </form>    </body></html>";
 
 int utf8_to_md()
 {
@@ -78,85 +88,97 @@ int main()
     CHECK(!rc, "Can't open database: %s", sqlite3_errmsg(db))
     rc = sqlite3_exec(
          db,
-         "CREATE TABLE IF NOT EXISTS session("
-         "    sid           INTEGER PRIMARY KEY,"
-         "    creation_time INTEGER NOT NULL DEFAULT (strftime('%s', 'now')));"
-         "CREATE TABLE IF NOT EXISTS user("
-         "    id   INTEGER PRIMARY KEY,"
-         "    name TEXT,"
-         "    pwd  TEXT);",
+         "CREATE TABLE IF  NOT EXISTS note("
+         "    id           INTEGER PRIMARY KEY,"
+         "    title        TEXT,"
+         "    content      TEXT);"
+         "CREATE TABLE IF  NOT EXISTS session("
+         "    id           INTEGER PRIMARY KEY,"
+         "    user_id      INTEGER NOT NULL,"
+         "    create_time  INTEGER NOT NULL DEFAULT (strftime('%s', 'now')));"
+         "CREATE TABLE IF  NOT EXISTS user("
+         "    id           INTEGER PRIMARY KEY,"
+         "    name         TEXT NOT NULL UNIQUE,"
+         "    pwd_hash     TEXT NOT NULL);"
+         "CREATE TABLE IF  NOT EXISTS log("
+         "    id           INTEGER PRIMARY KEY,"
+         "    time         INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),"
+         "    HTTP_COOKIE  TEXT,"
+         "    HTTP_USER_AGENT TEXT,"
+         "    QUERY_STRING TEXT);"
+         "CREATE TABLE IF  NOT EXISTS note_user_rel("
+         "    user_id      INTEGER NOT NULL,"
+         "    note_id      INTEGER NOT NULL,"
+         "    PRIMARY KEY(user_id, note_id),"
+         "    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,"
+         "    FOREIGN KEY(note_id) REFERENCES note(id) ON DELETE CASCADE);",
          0, 0, 0);
     CHECK(rc == SQLITE_OK, "Can't create tables: %d", rc)
 
-    // Print stdin
-    //char buf[1024] = {0};
-    //long len       = fread(buf, 1, 1024, stdin);
-    //printf("%s", buf);
-    //while (!feof(stdin))
-    //{
-    //    len = fread(buf, 1, 1024, stdin);
-    //    printf("%s", buf);
-    //}
-
-    // Check SID
+    // Assume initially that the user is not authenticated
     bool authenticated = false;
+
+    // Retrieve the submitted sid
     int64_t sid;
-    int sid_age = INT_MAX;
     get_sid_cookie(&sid);
-    if (sid == 0)
+
+    // Check if login request
+    const char* query_str = getenv("QUERY_STRING") ? getenv("QUERY_STRING") : "";
+    if (strstr(query_str, "login"))
     {
-        // Generate new sid
-        //generate_sid(&sid);
+        // Read submitted credentials from stdin
+        const char* content_len_str = getenv("CONTENT_LENGTH");
+        CHECK(content_len_str, "Invalid login request, no CONTENT_LENGTH defined.");
+        long content_len = 0;
+        sscanf(content_len_str, "%d", &content_len);
+        CHECK(content_len > 0, "Invalid login request, unexpected CONTENT_LENGTH: %d", content_len);
+        CHECK(content_len < 1024, "Invalid login request, CONTENT_LENGTH too large: %d", content_len);
+        char buf[1024] = {0};
+        long read_len = fread(buf, 1, content_len, stdin);
+        CHECK(read_len == content_len, "Invalid login request, could not read data (read: %d, CONTENT_LENGTH: %d)", read_len, content_len);
     }
+
+    // Not a login request, check session
     else
     {
-        // Look if the SID exists in the DB
+        // Get the age of the session from the DB
+        int sid_age = INT_MAX;
         char sql[1024];
-        sprintf(sql, "SELECT sid, (strftime('%%s', 'now') - creation_time) as age FROM session WHERE sid=%lld", sid);
+        sprintf(sql, "SELECT (strftime('%%s', 'now') - creation_time) as age FROM session WHERE sid=%lld", sid);
         rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
         rc = sqlite3_step(stmt);
         if (rc == SQLITE_ROW)
         {
-            sid = sqlite3_column_int64(stmt, 0);
-            sid_age = sqlite3_column_int(stmt, 1);
+            sid_age = sqlite3_column_int(stmt, 0);
         }
-    }
 
-    printf("HTTP/1.0 200 OK\n");
-    printf("Content-type: text/html\n\n");
-    printf("<p>QUERY_STRING: %s</p>", getenv("QUERY_STRING"));
-
-    if (strstr(getenv("QUERY_STRING"), "login"))
-    {
-        printf("login\n");
-        authenticated = true;
+        // Check if the session is valid
+        if (sid_age < 7 * 24 * 3600) authenticated = true;
     }
 
     if (authenticated)
     {
-        printf("logged in!\n");
+        // Header
+        printf("HTTP/1.0 200 OK\n");
+        printf("Content-type: text/html\n");
+        printf("\n");
+
+        // Content
+        printf("<html><head></head><body><p>logged in! show app</p></body></html>\n");
     }
     else
     {
-        // Set the session ID cookie
+        // Header
+        printf("HTTP/1.0 200 OK\n");
+        printf("Content-type: text/html\n");
+        generate_sid(&sid);
         printf("Set-Cookie: sid=%lld; Max-Age=60\n", sid);
+        printf("\n");
 
-        // Send the login page
+        // Content
         printf("%s\n", login_page);
     }
 
-    printf("\n");
-    printf("CONTENT_LENGHT: %s\n", getenv("CONTENT_LENGTH"));
-
-    // Print stdin
-    char buf[1024] = {0};
-    long len       = fread(buf, 1, 1024, stdin);
-    printf("len: %d, str: %s\n", len, buf);
-    while (!feof(stdin))
-    {
-        len = fread(buf, 1, 1024, stdin);
-        printf("%s\n", buf);
-    }
     return 0;
 }
 
