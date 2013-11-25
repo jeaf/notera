2aba6189ddbefdca9051d0723d7830721db426a5
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-11-25 15:05:01 -0600

    Add exception handling.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,17 @@
 
 using namespace std;
 
+void error(const char* msg, const char* file, const char* func, long line, ...)
+{
+    va_list args;
+    va_start(args, line);
+    char buf[2048];
+    vsprintf(buf, msg, args);
+    ostringstream oss;
+    oss << buf << " [" << file << "!" << func << ":" << line << "]";
+    throw runtime_error(oss.str());
+}
+
 template <typename Target, typename Source>
 Target lexical_cast(const Source& arg)
 {
@@ -44,20 +55,22 @@ public:
     };
 
     Sqlite() : db(NULL) {}
-    int exec(const char* sql_buf, int (*callback)(void*,int,char**,char**), void* arg1, char** errmsg)
+    void exec(const char* sql_buf, int (*callback)(void*,int,char**,char**), void* arg1, char** error_msg)
     {
-        return sqlite3_exec(db, sql_buf, callback, arg1, errmsg);
+        int rc = sqlite3_exec(db, sql_buf, callback, arg1, error_msg);
+        CHECK(rc == SQLITE_OK, "Can't execute: %s (%d)", errmsg(), rc)
     }
     shared_ptr<Stmt> prepare_v2(const char *zSql, int nByte, const char **pzTail)
     {
         sqlite3_stmt* stmt = NULL;
         int rc = sqlite3_prepare_v2(db, zSql, nByte, &stmt, pzTail);
-        (void)rc;
+        CHECK(rc == SQLITE_OK, "Can't prepare statement: %s", errmsg());
         return shared_ptr<Stmt>(new Stmt(stmt));
     }
-    int open(const char *filename)
+    void open(const char *filename)
     {
-        return sqlite3_open(filename, &db);
+        int rc = sqlite3_open(filename, &db);
+        CHECK(!rc, "Can't open database: %s", errmsg())
     }
     const char* errmsg()
     {
@@ -66,21 +79,6 @@ public:
     sqlite3* db;
     char sql[1024];
 };
-
-void error(const char* msg, const char* file, const char* func, long line, ...)
-{
-    va_list args;
-    va_start(args, line);
-    printf("HTTP/1.0 200 OK\n");
-    printf("Content-type: text/html\n");
-    printf("\n");
-    printf("<html><head></head><body>");
-    printf("<p>An error occurred while generating this page:</p>\n<p>");
-    vprintf(msg, args);
-    printf("</p>\n<p>At: %s!%s:%ld", file, func, line);
-    printf("</p>\n</body></html>\n");
-    exit(0);
-}
 
 void add_user(Sqlite& db, const char* username, const char* pwd)
 {
@@ -91,8 +89,7 @@ void add_user(Sqlite& db, const char* username, const char* pwd)
 
     sprintf(db.sql, "INSERT INTO user(name, pwd_hash) VALUES ('%s', '%02x%02x%02x%02x%02x');", username,
         sha.Message_Digest[0], sha.Message_Digest[1], sha.Message_Digest[2], sha.Message_Digest[3], sha.Message_Digest[4]);
-    int rc = db.exec(db.sql, NULL, NULL, NULL);
-    CHECK(rc == SQLITE_OK, "Can't insert user: %s", db.errmsg())
+    db.exec(db.sql, NULL, NULL, NULL);
 }
 
 int generate_sid(Sqlite& db, int64_t* oSid)
@@ -130,187 +127,197 @@ int get_sid_cookie(int64_t* oSid)
 
 int main()
 {
-    // Connect to the DB and create tables
-    Sqlite db;
-    int rc = db.open("db.sqlite3");
-    CHECK(!rc, "Can't open database: %s", db.errmsg())
-    rc = db.exec(
-         "CREATE TABLE IF  NOT EXISTS note("
-         "    id           INTEGER PRIMARY KEY,"
-         "    title        TEXT,"
-         "    content      TEXT);"
-         "CREATE TABLE IF  NOT EXISTS session("
-         "    id           INTEGER PRIMARY KEY,"
-         "    user_id      INTEGER NOT NULL,"
-         "    create_time  INTEGER NOT NULL DEFAULT (strftime('%s', 'now')));"
-         "CREATE TABLE IF  NOT EXISTS user("
-         "    id           INTEGER PRIMARY KEY,"
-         "    name         TEXT NOT NULL UNIQUE,"
-         "    pwd_hash     TEXT NOT NULL);"
-         "CREATE TABLE IF  NOT EXISTS log("
-         "    id           INTEGER PRIMARY KEY,"
-         "    time         INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),"
-         "    HTTP_COOKIE  TEXT,"
-         "    HTTP_USER_AGENT TEXT,"
-         "    QUERY_STRING TEXT);"
-         "CREATE TABLE IF  NOT EXISTS note_user_rel("
-         "    user_id      INTEGER NOT NULL,"
-         "    note_id      INTEGER NOT NULL,"
-         "    PRIMARY KEY(user_id, note_id),"
-         "    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,"
-         "    FOREIGN KEY(note_id) REFERENCES note(id) ON DELETE CASCADE);",
-         0, 0, 0);
-    CHECK(rc == SQLITE_OK, "Can't create tables: %d", rc)
-
-    // Create a user
-    //add_user(db, "abc", "def");
-    //return 0;
-
-    // Assume initially that the user is not authenticated
-    bool authenticated = false;
-
-    // Retrieve the submitted sid
-    int64_t sid;
-    get_sid_cookie(&sid);
-
-    // Check if login request
-    const char* query_str = getenv("QUERY_STRING") ? getenv("QUERY_STRING") : "";
-    if (strstr(query_str, "login"))
+    try
     {
-        // Read submitted credentials from stdin
-        const char* content_len_str = getenv("CONTENT_LENGTH");
-        CHECK(content_len_str, "Invalid login request, no CONTENT_LENGTH defined.");
-        long content_len = 0;
-        sscanf(content_len_str, "%ld", &content_len);
-        CHECK(content_len > 0, "Invalid login request, unexpected CONTENT_LENGTH: %d", content_len);
-        CHECK(content_len < 1024, "Invalid login request, CONTENT_LENGTH too large: %d", content_len);
-        char buf[1024] = {0};
-        long read_len = fread(buf, 1, content_len, stdin);
-        CHECK(read_len == content_len, "Invalid login request, could not read data (read: %d, CONTENT_LENGTH: %d)", read_len, content_len);
+        // Connect to the DB and create tables
+        Sqlite db;
+        db.open("db.sqlite3");
+        db.exec(
+             "CREATE TABLE IF  NOT EXISTS note("
+             "    id           INTEGER PRIMARY KEY,"
+             "    title        TEXT,"
+             "    content      TEXT);"
+             "CREATE TABLE IF  NOT EXISTS session("
+             "    id           INTEGER PRIMARY KEY,"
+             "    user_id      INTEGER NOT NULL,"
+             "    create_time  INTEGER NOT NULL DEFAULT (strftime('%s', 'now')));"
+             "CREATE TABLE IF  NOT EXISTS user("
+             "    id           INTEGER PRIMARY KEY,"
+             "    name         TEXT NOT NULL UNIQUE,"
+             "    pwd_hash     TEXT NOT NULL);"
+             "CREATE TABLE IF  NOT EXISTS log("
+             "    id           INTEGER PRIMARY KEY,"
+             "    time         INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),"
+             "    HTTP_COOKIE  TEXT,"
+             "    HTTP_USER_AGENT TEXT,"
+             "    QUERY_STRING TEXT);"
+             "CREATE TABLE IF  NOT EXISTS note_user_rel("
+             "    user_id      INTEGER NOT NULL,"
+             "    note_id      INTEGER NOT NULL,"
+             "    PRIMARY KEY(user_id, note_id),"
+             "    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,"
+             "    FOREIGN KEY(note_id) REFERENCES note(id) ON DELETE CASCADE);",
+             0, 0, 0);
 
-        // Find user name
-        char user[1024] = {0};
-        char* user_it = user;
-        char* buf_it = strstr(buf, "username");
-        if (buf_it)
+        // Create a user
+        //add_user(db, "abc", "def");
+        //return 0;
+
+        // Assume initially that the user is not authenticated
+        bool authenticated = false;
+
+        // Retrieve the submitted sid
+        int64_t sid;
+        get_sid_cookie(&sid);
+
+        // Check if login request
+        const char* query_str = getenv("QUERY_STRING") ? getenv("QUERY_STRING") : "";
+        if (strstr(query_str, "login"))
         {
-            buf_it += strlen("username") + 1;
-            while (*buf_it && *buf_it != '&') *user_it++ = *buf_it++;
+            // Read submitted credentials from stdin
+            const char* content_len_str = getenv("CONTENT_LENGTH");
+            CHECK(content_len_str, "Invalid login request, no CONTENT_LENGTH defined.");
+            long content_len = 0;
+            sscanf(content_len_str, "%ld", &content_len);
+            CHECK(content_len > 0, "Invalid login request, unexpected CONTENT_LENGTH: %d", content_len);
+            CHECK(content_len < 1024, "Invalid login request, CONTENT_LENGTH too large: %d", content_len);
+            char buf[1024] = {0};
+            long read_len = fread(buf, 1, content_len, stdin);
+            CHECK(read_len == content_len, "Invalid login request, could not read data (read: %d, CONTENT_LENGTH: %d)", read_len, content_len);
+
+            // Find user name
+            char user[1024] = {0};
+            char* user_it = user;
+            char* buf_it = strstr(buf, "username");
+            if (buf_it)
+            {
+                buf_it += strlen("username") + 1;
+                while (*buf_it && *buf_it != '&') *user_it++ = *buf_it++;
+            }
+
+            // Find submitted auth token (hash of username, hashed_pwd, sid)
+            char auth_token[1024] = {0};
+            char* auth_token_it = auth_token;
+            buf_it = strstr(buf, "auth_token");
+            if (buf_it)
+            {
+                buf_it += strlen("auth_token") + 1;
+                while (*buf_it && *buf_it != '&') *auth_token_it++ = *buf_it++;
+            }
+
+            // Construct the expected auth token
+            //char exp_token[1024];
+            Sha1 sha(user);
+            sprintf(db.sql, "SELECT id,pwd_hash FROM user WHERE name='%s'", user);
+            shared_ptr<Sqlite::Stmt> stmt = db.prepare_v2(db.sql, -1, 0);
+            int rc = sqlite3_step(stmt->stmt_);
+            char hash_pwd[1024] = {0};
+            long long user_id = -1;
+            if (rc == SQLITE_ROW)
+            {
+                user_id = sqlite3_column_int64(stmt->stmt_, 0);
+                strcpy(hash_pwd, reinterpret_cast<const char*>(sqlite3_column_text(stmt->stmt_, 1)));
+            }
+            sha.update(hash_pwd);
+            string sid_str = lexical_cast<string>(sid);
+            sha.update(sid_str);
+            sha.result();
+            unsigned int* s = sha.Message_Digest;
+            char expected_auth_token[1024] = {0};
+            sprintf(expected_auth_token, "%02x%02x%02x%02x%02x", s[0], s[1], s[2], s[3], s[4]);
+
+            // Compare the expected auth token to the submitted one
+            if (strcmp(expected_auth_token, auth_token) == 0)
+            {
+                // User is authenticated, store the session
+                sprintf(db.sql, "INSERT INTO session(id, user_id) VALUES(%ld, %lld)", sid, user_id);
+                db.exec(db.sql, NULL, NULL, NULL);
+                printf("HTTP/1.0 200 OK\n");
+                printf("Content-type: text/html\n");
+                printf("\n");
+                printf("<html><head></head><body><p>Login successful!</p></body></html>\n");
+                authenticated = true;
+            }
+            else
+            {
+                printf("HTTP/1.0 200 OK\n");
+                printf("Content-type: text/html\n");
+                printf("\n");
+                printf("<html><head></head><body><p>Login FAIL, user: %s, pwd_hash: %s, tok: %s, sid: %s</p></body></html>\n",
+                       user, hash_pwd, sid_str.c_str(), auth_token);
+            }
+
+            return 0;
         }
 
-        // Find submitted auth token (hash of username, hashed_pwd, sid)
-        char auth_token[1024] = {0};
-        char* auth_token_it = auth_token;
-        buf_it = strstr(buf, "auth_token");
-        if (buf_it)
+        // Not a login request, check session
+        else
         {
-            buf_it += strlen("auth_token") + 1;
-            while (*buf_it && *buf_it != '&') *auth_token_it++ = *buf_it++;
+            // Get the age of the session from the DB
+            int sid_age = INT_MAX;
+            sprintf(db.sql, "SELECT (strftime('%%s', 'now') - create_time) as age FROM session WHERE id=%ld", sid);
+            shared_ptr<Sqlite::Stmt> stmt = db.prepare_v2(db.sql, -1, 0);
+            int rc = sqlite3_step(stmt->stmt_);
+            if (rc == SQLITE_ROW)
+            {
+                sid_age = sqlite3_column_int(stmt->stmt_, 0);
+            }
+
+            // Check if the session is valid
+            if (sid_age < 7 * 24 * 3600) authenticated = true;
         }
 
-        // Construct the expected auth token
-        //char exp_token[1024];
-        Sha1 sha(user);
-        sprintf(db.sql, "SELECT id,pwd_hash FROM user WHERE name='%s'", user);
-        shared_ptr<Sqlite::Stmt> stmt = db.prepare_v2(db.sql, -1, 0);
-        rc = sqlite3_step(stmt->stmt_);
-        char hash_pwd[1024] = {0};
-        long long user_id = -1;
-        if (rc == SQLITE_ROW)
+        if (authenticated)
         {
-            user_id = sqlite3_column_int64(stmt->stmt_, 0);
-            strcpy(hash_pwd, reinterpret_cast<const char*>(sqlite3_column_text(stmt->stmt_, 1)));
-        }
-        sha.update(hash_pwd);
-        string sid_str = lexical_cast<string>(sid);
-        sha.update(sid_str);
-        sha.result();
-        unsigned int* s = sha.Message_Digest;
-        char expected_auth_token[1024] = {0};
-        sprintf(expected_auth_token, "%02x%02x%02x%02x%02x", s[0], s[1], s[2], s[3], s[4]);
-
-        // Compare the expected auth token to the submitted one
-        if (strcmp(expected_auth_token, auth_token) == 0)
-        {
-            // User is authenticated, store the session
-            sprintf(db.sql, "INSERT INTO session(id, user_id) VALUES(%ld, %lld)", sid, user_id);
-            int rc = db.exec(db.sql, NULL, NULL, NULL);
-            CHECK(rc == SQLITE_OK, "Can't insert session: %d", rc);
+            // Header
             printf("HTTP/1.0 200 OK\n");
             printf("Content-type: text/html\n");
             printf("\n");
-            printf("<html><head></head><body><p>Login successful!</p></body></html>\n");
-            authenticated = true;
+
+            // Content
+            FILE* login_page = fopen("app.html", "r");
+            char buf[2048] = {0};
+            long len = fread(buf, 1, sizeof(buf) - 1, login_page);
+            while (len > 0)
+            {
+                buf[len] = 0;
+                printf("%s", buf);
+                len = fread(buf, 1, sizeof(buf) - 1, login_page);
+            }
         }
         else
         {
+            // Header
             printf("HTTP/1.0 200 OK\n");
             printf("Content-type: text/html\n");
+            generate_sid(db, &sid);
+            printf("Set-Cookie: sid=%ld; Max-Age=%d\n", sid, 7 * 24 * 3600);
             printf("\n");
-            printf("<html><head></head><body><p>Login FAIL, user: %s, pwd_hash: %s, tok: %s, sid: %s</p></body></html>\n",
-                   user, hash_pwd, sid_str.c_str(), auth_token);
+
+            // Content
+            FILE* login_page = fopen("login.html", "r");
+            char buf[2048] = {0};
+            long len = fread(buf, 1, sizeof(buf) - 1, login_page);
+            while (len > 0)
+            {
+                buf[len] = 0;
+                printf("%s", buf);
+                len = fread(buf, 1, sizeof(buf) - 1, login_page);
+            }
         }
 
         return 0;
     }
-
-    // Not a login request, check session
-    else
+    catch (const std::exception& ex)
     {
-        // Get the age of the session from the DB
-        int sid_age = INT_MAX;
-        sprintf(db.sql, "SELECT (strftime('%%s', 'now') - create_time) as age FROM session WHERE id=%ld", sid);
-        shared_ptr<Sqlite::Stmt> stmt = db.prepare_v2(db.sql, -1, 0);
-        CHECK(rc == SQLITE_OK, "Session ID retrieval failed: %d", rc);
-        rc = sqlite3_step(stmt->stmt_);
-        if (rc == SQLITE_ROW)
-        {
-            sid_age = sqlite3_column_int(stmt->stmt_, 0);
-        }
-
-        // Check if the session is valid
-        if (sid_age < 7 * 24 * 3600) authenticated = true;
-    }
-
-    if (authenticated)
-    {
-        // Header
         printf("HTTP/1.0 200 OK\n");
         printf("Content-type: text/html\n");
         printf("\n");
-
-        // Content
-        FILE* login_page = fopen("app.html", "r");
-        char buf[2048] = {0};
-        long len = fread(buf, 1, sizeof(buf) - 1, login_page);
-        while (len > 0)
-        {
-            buf[len] = 0;
-            printf("%s", buf);
-            len = fread(buf, 1, sizeof(buf) - 1, login_page);
-        }
-    }
-    else
-    {
-        // Header
-        printf("HTTP/1.0 200 OK\n");
-        printf("Content-type: text/html\n");
-        generate_sid(db, &sid);
-        printf("Set-Cookie: sid=%ld; Max-Age=%d\n", sid, 7 * 24 * 3600);
-        printf("\n");
-
-        // Content
-        FILE* login_page = fopen("login.html", "r");
-        char buf[2048] = {0};
-        long len = fread(buf, 1, sizeof(buf) - 1, login_page);
-        while (len > 0)
-        {
-            buf[len] = 0;
-            printf("%s", buf);
-            len = fread(buf, 1, sizeof(buf) - 1, login_page);
-        }
+        printf("<html><head></head><body>");
+        printf("<p>An error occurred while generating this page:</p>\n<p>");
+        printf("%s</p>\n</body></html>\n", ex.what());
     }
 
-    return 0;
+    return 1;
 }
 
