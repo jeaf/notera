43818c5ed6959a85aa3ef79dc89f425f11ba3208
Francois Jeannotte <francois.jeannotte@gmail.com>
2014-01-20 16:42:01 -0600

    Add support for PUT request in server.py.

--- server.py
+++ server.py
@@ -4,13 +4,24 @@ import cgitb; cgitb.enable()  ## This line enables CGI error reporting
 import os
 import os.path
 
+class MyHandler(CGIHTTPServer.CGIHTTPRequestHandler):
+    def do_PUT(self):
+        """
+        Serve a PUT request.
+        This is only implemented for CGI scripts.
+        """
+        if self.is_cgi():
+            self.run_cgi()
+        else:
+            self.send_error(501, "Can only PUT to CGI scripts")
+
 # Go to previous dir to simulate being on the host, and have the "notera"
 # folder on the URL when accessing the server. e.g., instead of
 # http://host/cgi-bin/api.cgi, have http://host.com/notera/cgi-bin/api.cgi.
 os.chdir("..")
 
 server = BaseHTTPServer.HTTPServer
-handler = CGIHTTPServer.CGIHTTPRequestHandler
+handler = MyHandler
 handler.cgi_directories = ["/notera/cgi-bin"]
 server_address = ("", 8000)
 
